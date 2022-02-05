fbb17114d0ee34bb54296fb63221054c8ef0ca0b
merelyghost <itmygalya@gmail.com>
2022-02-05 18:33:27 +0300

    append push / pop functions

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -1,5 +1,7 @@
 #include "vector.h"
 
+
+// first ---------------------------
 vector createVector(size_t n) {
     int *a;
     if (n == 0) {
@@ -42,4 +44,36 @@ void deleteVector(vector *v) {
     free(v->data);
     v->capacity = 0;
     v->size = 0;
+}
+
+// second --------------------------
+
+bool isEmpty(vector *v) {
+    return v->size == 0;
+}
+
+bool isFull(vector *v) {
+    return v->size == v->capacity;
+}
+
+int getVectorValue(vector *v, size_t i) {
+    return (i < v->size) ? v->data[i] : -1;
+}
+
+void pushBack(vector *v, int x) {
+    if (v->capacity == 0)
+        reserve(v, 1);
+    else if (isFull(v)) {
+        reserve(v, v->capacity * 2);
+    }
+    v->data[v->size] = x;
+    v->size++;
+}
+
+void popBack(vector *v) {
+    if (isEmpty(v)) {
+        fprintf(stderr, "empty vector ");
+        exit(1);
+    }
+    v->size--;
 }
--- main.c
+++ main.c
@@ -1,8 +1,63 @@
-#include <stdlib.h>
+#include <assert.h>
 #include "libs/data_structures/vector/vector.h"
 
-int main() {
-    vector v = createVector(SIZE_MAX);
 
-    return 0;
+void test_getVectorValue_EmptyVector() {
+    vector v = createVector(0);
+    assert(getVectorValue(&v, 2) == -1);
+}
+
+void test_getVectorValue_NotEmptyVector() {
+    vector v = createVector(5);
+    for (int i = 1; i < 5; i++) {
+        pushBack(&v, i);
+    }
+    assert(getVectorValue(&v, 2) == 3);
+    assert(getVectorValue(&v, 4) == -1);
+}
+
+void test_pushBack_emptyVector() {
+    vector v = createVector(0);
+    pushBack(&v, 5);
+
+    assert(v.capacity == 1);
+    assert(v.size == 1);
+    assert(v.data[0] == 5);
+}
+
+void test_pushBack_fullVector() {
+    vector v = createVector(3);
+    for (int i = 1; i < 5; i++) {
+        pushBack(&v, i);
+    }
+
+    assert(v.capacity == 6);
+    assert(v.size == 4);
+    assert(v.data[3] == 4);
+}
+
+void test_popBack_notEmptyVector() {
+    vector v = createVector(0);
+    pushBack(&v, 10);
+    assert(v.size == 1);
+
+    popBack(&v);
+    assert(v.size == 0);
+    assert(v.capacity == 1);
+}
+
+void test() {
+    test_getVectorValue_EmptyVector();
+    test_getVectorValue_NotEmptyVector();
+    test_pushBack_emptyVector();
+    test_pushBack_fullVector();
+    test_popBack_notEmptyVector();
+
+    printf("everything is ok");
+}
+
+
+
+int main() {
+    test();
 }
