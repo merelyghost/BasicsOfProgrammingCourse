be3100c5029ad66e4bb2041c4703fc0e2dc69d58
merelyghost <itmygalya@gmail.com>
2022-02-14 20:00:54 +0300

    mem_usage, input/output, swap functions

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -1,2 +1,70 @@
+#include "matrix.h"
 
+void checkIndexOutOfRangeError(size_t size, size_t index) {
+    if (index >= size) {
+        fprintf(stderr, "IndexError: a[%zu] is not exists", index);
+        exit(1);
+    }
+}
 
+matrix getMemMatrix(int nRows, int nCols) {
+    int **values = (int **) malloc(sizeof(int *) * nRows);
+    for (int i = 0; i < nRows; i++)
+        values[i] = (int *) malloc(sizeof(int) * nCols);
+    return (matrix) {values, nRows, nCols};
+}
+
+matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols) {
+    matrix *ms = (matrix *) malloc(sizeof(matrix) * nMatrices);
+    for (int i = 0; i < nMatrices; i++)
+        ms[i] = getMemMatrix(nRows, nCols);
+    return ms;
+}
+
+void freeMemMatrix(matrix m) {
+    for (int i = 0; i < m.nRows; i++)
+        free(m.values[i]);
+    free(m.values);
+}
+
+void freeMemMatrices(matrix *ms, int nMatrices) {
+    for (int i = 0; i < nMatrices; i++)
+        freeMemMatrix(ms[i]);
+    free(ms);
+}
+
+void inputMatrix(matrix m) {
+    for (int i = 0; i < m.nRows; i++)
+        inputArray(m.values[i], m.nCols);
+}
+
+void inputMatrices(matrix *ms, int nMatrices) {
+    for (int i = 0; i < nMatrices; i++)
+        inputMatrix(ms[i]);
+}
+
+void outputMatrix(matrix m) {
+    for (int i = 0; i < m.nRows; i++)
+        outputArray(m.values[i], m.nCols);
+    printf("\n");
+}
+
+void outputMatrices(matrix *ms, int nMatrices) {
+    for (int i = 0; i < nMatrices; i++)
+        outputMatrix(ms[i]);
+}
+
+void swapRows(matrix m, int i1, int i2) {
+    checkIndexOutOfRangeError(m.nRows, i1);
+    checkIndexOutOfRangeError(m.nRows, i2);
+
+    swap(&m.values[i1], &m.values[i2], sizeof(int *));
+}
+
+void swapColumns(matrix m, int j1, int j2) {
+    checkIndexOutOfRangeError(m.nCols, j1);
+    checkIndexOutOfRangeError(m.nCols, j2);
+
+    for (int i = 0; i < m.nRows; i++)
+        swap(&m.values[i][j1], &m.values[i][j2], sizeof(int));
+}
--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -1,6 +1,11 @@
 #ifndef COURSE_MATRIX_H
 #define COURSE_MATRIX_H
 
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include "../../algorithms/array/array.h"
+
 typedef struct matrix {
     int **values; // элементы матрицы
     int nRows;    // количество рядов
@@ -13,3 +18,26 @@ typedef struct position {
 } position;
 
 #endif
+
+// размещает в динамической памяти матрицу размером nRows на nCols
+matrix getMemMatrix(int nRows, int nCols);
+// размещает в динамической памяти массив из nMatrices матриц размером nRows на nCols
+matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols);
+// освобождает память, выделенную под хранение матрицы m
+void freeMemMatrix(matrix m);
+// освобождает память, выделенную под хранение массива ms из nMatrices матриц
+void freeMemMatrices(matrix *ms, int nMatrices);
+
+// ввод матрицы m
+void inputMatrix(matrix m);
+// ввод массива из nMatrices матриц, хранящейся по адресу ms
+void inputMatrices(matrix *ms, int nMatrices);
+// вывод матрицы m
+void outputMatrix(matrix m);
+// вывод массива из nMatrices матриц, хранящейся по адресу ms
+void outputMatrices(matrix *ms, int nMatrices);
+
+// обмен строк с порядковыми номерами i1 и i2 в матрице m
+void swapRows(matrix m, int i1, int i2);
+// обмен колонок с порядковыми номерами j1 и j2 в матрице m
+void swapColumns(matrix m, int j1, int j2);
