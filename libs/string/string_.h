58551234ecdf0337646e4b458765344251f76696
merelyghost <itmygalya@gmail.com>
2022-03-01 23:17:44 +0300

    append functions

--- libs/string/string_.h
+++ libs/string/string_.h
@@ -1,8 +1,57 @@
-//
-// Created by Семья Перминовых on 01.03.2022.
-//
-
 #ifndef COURSE_STRING__H
 #define COURSE_STRING__H
 
-#endif //COURSE_STRING__H
+#include <stdio.h>
+#include <ctype.h>
+#include <memory.h>
+
+// возвращает количество символов в строке, не считая ноль-символ
+size_t strlen_(const char *begin);
+
+// возвращает указатель на первый элемент с кодом ch,
+// расположенным на ленте памяти между адресами begin и end не включая end,
+// если символ не найден, возвращается значение end
+char *find(char *begin, const char *end, int ch);
+
+// возвращает указатель на первый символ, отличный от пробельных,
+// расположенный на ленте памяти, начиная с begin и заканчивая ноль-символом,
+// если символ не найден, возвращается адрес первого ноль-символа
+char *findNonSpace(char *begin);
+
+// возвращает указатель на первый пробельный символ,
+// расположенный на ленте памяти начиная с адреса begin,
+// если символ не найден, возвращается адрес первого ноль-символа
+char *findSpace(char *begin);
+
+// возвращает указатель на первый справа символ, отличный от пробельных,
+// расположенный на ленте памяти, начиная с rbegin (последний символ строки, за которым следует ноль-символ)
+// и заканчивая rend (адрес символа перед началом строки),
+// если символ не найден, возвращается адрес rend
+char *findNonSpaceReverse(char *rbegin, const char *rend);
+
+// возвращает указатель на первый пробельный символ справа,
+// расположенный на ленте памяти, начиная с rbegin и заканчивая rend,
+// если символ не найден, возвращается адрес rend
+char *findSpaceReverse(char *rbegin, const char *rend);
+
+
+// возвращает значение 0, если lhs и rhs равны,
+// отрицательное значение, если lhs располагается до rhs в лексикографическом порядке,
+// иначе положительное значение
+int strcmp(const char *lhs, const char *rhs);
+
+// записывает по адресу beginDestination фрагмент памяти, начиная с адреса beginSource до endSource,
+// возвращает указатель на следующий свободный фрагмент памяти в destination
+char* copy(const char *beginSource, const char *endSource, char *beginDestination);
+
+// записывает по адресу beginDestination элементы из фрагмента памяти начиная с beginSource
+// заканчивая endSource, удовлетворяющие функции-предикату f,
+// возвращает указатель на следующий свободный для записи фрагмент в памяти
+char* copyIf(char *beginSource, const char *endSource, char *beginDestination, int (*f)(int));
+
+// записывает по адресу beginDestination элементы из фрагмента памяти начиная с rbeginSource
+// заканчивая rendSource, удовлетворяющие функции-предикату f,
+// возвращает значение beginDestination по окончанию работы функции
+char* copyIfReverse(char *rbeginSource, const char *rendSource, char *beginDestination, int (*f)(int));
+
+#endif
